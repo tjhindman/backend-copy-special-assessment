403db01e6311f7ebb88f78e9a9620b9bfc77f406
TJ Hindman <thind131@gmail.com>
2018-12-02 21:04:31 -0500

    part a complete

--- copyspecial.py
+++ copyspecial.py
@@ -18,17 +18,38 @@ import argparse
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def collect_paths(dir):
+    file_dir = os.listdir(dir)
+    match_file = [os.path.abspath(file) for file in file_dir if re.search(r'__\w+__', file)]
+
+    return match_file
+
+
+def copy_files(path_list, to_dir):
+    if os.path.exists(to_dir):
+        
+
 
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('from_dir', help='positional argument defined by user input')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
 
+    print args.from_dir
+
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
+    paths = collect_paths(args.from_dir)
+
+    if not paths:
+        print 'No bueno'
+    elif paths and args.todir:
+        copy_files(paths, args.todir)
+
 
     # Parsing command line arguments is a must-have skill.
     # This is input data validation.  If something is wrong (or missing) with any
